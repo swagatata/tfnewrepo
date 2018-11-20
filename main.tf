1f7e04ca3b8a4efd492022a0fd8146a01a92bc7a
Swagat Konchada <swagatata@gmail.com>
2018-11-20 21:17:32 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 # global/main.tf
 # same script 3 different envs
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+
+variable "aws_credential_filepath" {
+  
+}
+
+variable "aws_profile" {
+  
+}
 
 provider "aws" {
   region  = "us-east-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  shared_credentials_file = "${var.aws_credential_filepath}"
+  profile = "${var.aws_profile}"
 }
 
 terraform {
@@ -17,16 +23,16 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config {
-    bucket = "personal-remote-state-dev"
-    key    = "terraform.tfstate"
-    region = "us-east-1"    
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-  }
-}
+# data "terraform_remote_state" "network" {
+#   backend = "s3"
+#   config {
+#     bucket = "personal-remote-state-dev"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"    
+#     access_key = "${var.aws_access_key}"
+#     secret_key = "${var.aws_secret_key}"
+#   }
+# }
 
 variable "count" {}
 resource "aws_instance" "tf_instance" {
@@ -38,6 +44,6 @@ resource "aws_instance" "tf_instance" {
   }
 }
 
-output "backend_config" {
-  value = "${data.terraform_remote_state.network.config}"
-}
+# output "backend_config" {
+#   value = "${data.terraform_remote_state.network.config}"
+# }
